da8ec847c4695b8238391998f9f92917e0374d5c
aerifyio <40509131+aerifyio@users.noreply.github.com>
2018-06-23 16:01:33 -0400

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_IRw4g9Wrq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '107sjdu70fqpki8mda20clu4ka', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_A2Pf47Kzx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'd6tr3vso6j7kijgpkig4vf0ic', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://s90zlwu271.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'earl@eeg3.net'
+        email: 'admin@aerify.io'
     }
 };
