cf2f5fe72c18485be10cd81fbfc6bc435c0e665a
aerifyio <40509131+aerifyio@users.noreply.github.com>
2018-06-23 15:46:23 -0400

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -5,7 +5,7 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qj8fvp8b7d.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://s90zlwu271.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'earl@eeg3.net'
